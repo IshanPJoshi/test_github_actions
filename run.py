a27a72af06e7ae0199662210737f9714d9791348
Ishan Joshi <55596316+IshanPJoshi@users.noreply.github.com>
2021-08-27 18:13:27 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 food = os.environ.get('FAVORITE_FOOD')
 if(food == "Pizza"):
-        print("My favorite food is", "Pizza)
+        print("My favorite food is Pizza")
 else:
         print("I guess I don't know")
