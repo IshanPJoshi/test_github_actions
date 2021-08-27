86ab2cfc5215b2633998e8f512db1c92bbaafc86
Ishan Joshi <55596316+IshanPJoshi@users.noreply.github.com>
2021-08-27 18:19:20 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import os
+import json
+
 food = os.environ.get('FAVORITE_FOOD')
 if(food == "Pizza"):
         print("My favorite food is Pizza")
+        f = open('data.json')
+        print(json.load(f))
 else:
         print("I guess I don't know")
